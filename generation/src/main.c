608aa908a1b65e71fcabe4132447e7de3fb84535
Nicolas Reyland <nicolas@reyland.dev>
2022-11-23 23:55:42 +0100

    refactor: moved -g and -t src code into functions

--- generation/src/main.c
+++ generation/src/main.c
@@ -8,6 +8,12 @@
 #include "primality_test.h"
 #include "random.h"
 
+#define EXIT_CODE_SUCCESS 0
+#define EXIT_CODE_FAIlURE 2
+
+#define EXIT_CODE_NOT_PRIME 0
+#define EXIT_CODE_IS_PRIME 1
+
 #define CMD_FLAGS_GEN 0b000001
 #define CMD_FLAGS_HEX 0b000010
 #define CMD_FLAGS_TST 0b000100
@@ -19,112 +25,118 @@ static unsigned parse_args(int argc, char **argv, char *buffer);
 
 static void usage_msg(void);
 
+static int exec_generate_prime(unsigned flags, char *buffer);
+
+static int exec_primality_test(unsigned flags, char *buffer);
+
 int main(int argc, char **argv)
 {
     char buffer[4096];
     unsigned flags = parse_args(argc, argv, buffer);
 
-    int exit_code = 0;
+    int exit_code = EXIT_CODE_SUCCESS;
 
-    if (flags & CMD_FLAGS_ERR || flags & CMD_FLAGS_HLP)
+    if (flags & (CMD_FLAGS_ERR | CMD_FLAGS_HLP))
     {
         usage_msg();
-        exit(flags & CMD_FLAGS_ERR ? EXIT_FAILURE : EXIT_SUCCESS);
+        exit(flags & CMD_FLAGS_ERR ? EXIT_CODE_FAIlURE : EXIT_CODE_SUCCESS);
     }
 
     initialize_prng();
 
     /* Prime Number Generation */
     if (flags & CMD_FLAGS_GEN)
-    {
-        char *endptr = NULL;
-        long length = strtol(buffer, &endptr, 10);
-        if (*endptr != 0)
-        {
-            LOG_ERROR("Invalid integer: %s (only base allwed is 10)\n", buffer)
-            usage_msg();
-            exit_code = 2;
-        }
-        else
-        {
-            BIGNUM *p = generate_prime(length);
-            if (p == NULL)
-            {
-                LOG_ERROR("Failed to generate prime with length %s", buffer);
-                exit_code = 1;
-            }
-            else
-            {
-                char *p_str =
-                    flags & CMD_FLAGS_HEX ? BN_bn2hex(p) : BN_bn2dec(p);
-                printf("%s\n", p_str);
-                OPENSSL_free(p_str);
-                BN_free(p);
-                exit_code = 0;
-            }
-        }
-    }
+        exit_code = exec_generate_prime(flags, buffer);
     /* Primality Testing */
     else if (flags & CMD_FLAGS_TST)
-    {
-        BIGNUM *n = NULL;
-        int (*bn_read_fn)(BIGNUM * *a, const char *str);
-
-        if (flags & CMD_FLAGS_DEC)
-        {
-            LOG_DEBUG("Reading %s as decimal value", buffer);
-            bn_read_fn = BN_dec2bn;
-        }
-        else
-        {
-            LOG_DEBUG("Reading %s as hex value (default)", buffer);
-            bn_read_fn = BN_hex2bn;
-        }
-        size_t num_read = (*bn_read_fn)(&n, buffer);
-        size_t num_chars_to_read = strlen(buffer);
-        if (num_read == 0 || num_read < num_chars_to_read)
-        {
-            LOG_ERROR("Could not read given prime number \"%s\"", buffer)
-            exit_code = 2;
-        }
-        else
-        {
-            int success = primality_test(n);
-            switch (success)
-            {
-            case 1: {
-                LOG_INFO("%s (%s) is a prime number", buffer,
-                         flags & CMD_FLAGS_DEC ? "dec" : "hex");
-                exit_code = 1;
-            }
-            break;
-            case 0: {
-                LOG_INFO("%s (%s) is NOT a prime number", buffer,
-                         flags & CMD_FLAGS_DEC ? "dec" : "hex");
-                exit_code = 0;
-            }
-            break;
-            default: {
-                LOG_WARN("primality check exited with a failure status for %s",
-                         buffer)
-                exit_code = 0;
-            }
-            break;
-            }
-            BN_free(n);
-        }
-    }
+        exit_code = exec_primality_test(flags, buffer);
     /* No command input */
     else
     {
         usage_msg();
-        exit_code = 0;
+        exit_code = EXIT_CODE_SUCCESS;
     }
 
     // clean up
     CRYPTO_cleanup_all_ex_data();
 
     return exit_code;
+}
+
+int exec_generate_prime(unsigned flags, char *buffer)
+{
+    char *endptr = NULL;
+    long length = strtol(buffer, &endptr, 10);
+
+    if (*endptr != 0)
+    {
+        LOG_ERROR("Invalid integer: %s (only base allwed is 10)\n", buffer)
+        usage_msg();
+        return EXIT_CODE_FAIlURE;
+    }
+
+    BIGNUM *p = generate_prime(length);
+    if (p == NULL)
+    {
+        LOG_ERROR("Failed to generate prime with length %s", buffer);
+        return EXIT_CODE_FAIlURE;
+    }
+    else
+    {
+        char *p_str = flags & CMD_FLAGS_HEX ? BN_bn2hex(p) : BN_bn2dec(p);
+        printf("%s\n", p_str);
+        OPENSSL_free(p_str);
+        BN_free(p);
+    }
+
+    return EXIT_CODE_SUCCESS;
+}
+
+int exec_primality_test(unsigned flags, char *buffer)
+{
+    BIGNUM *n = NULL;
+    int (*bn_read_fn)(BIGNUM * *a, const char *str);
+
+    if (flags & CMD_FLAGS_DEC)
+    {
+        LOG_DEBUG("Reading %s as decimal value", buffer);
+        bn_read_fn = BN_dec2bn;
+    }
+    else
+    {
+        LOG_DEBUG("Reading %s as hex value (default)", buffer);
+        bn_read_fn = BN_hex2bn;
+    }
+    size_t num_read = (*bn_read_fn)(&n, buffer);
+    size_t num_chars_to_read = strlen(buffer);
+    if (num_read == 0 || num_read < num_chars_to_read)
+        LOG_ERROR("Could not read given prime number \"%s\"", buffer)
+    else
+    {
+        int success = primality_test(n);
+        BN_free(n);
+
+        switch (success)
+        {
+        case 1: {
+            LOG_INFO("%s (%s) is a prime number", buffer,
+                     flags & CMD_FLAGS_DEC ? "dec" : "hex");
+            return EXIT_CODE_IS_PRIME;
+        }
+        case 0: {
+            LOG_INFO("%s (%s) is NOT a prime number", buffer,
+                     flags & CMD_FLAGS_DEC ? "dec" : "hex");
+            return EXIT_CODE_NOT_PRIME;
+        }
+        default: {
+            LOG_WARN("primality check exited with a failure status for %s",
+                     buffer)
+            return EXIT_CODE_FAIlURE;
+        }
+        }
+    }
+
+    return EXIT_CODE_FAIlURE;
 }
 
 static void set_verbosity(char *arg);
